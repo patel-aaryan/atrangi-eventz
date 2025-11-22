54f4cbe37b7e4b23ca7510c748b66115de37ea0e
Aaryan Patel <a52patel@uwaterloo.ca>
2025-11-22 13:15:12 -0500

    orders and tickets tabl

--- migrations/sqls/20251116041333-ticket-table-down.sql
+++ migrations/sqls/20251116041333-ticket-table-down.sql
@@ -8,9 +8,14 @@ DROP TRIGGER IF EXISTS update_tickets_updated_at ON tickets;
 -- Drop functions
 DROP FUNCTION IF EXISTS generate_ticket_code();
 
+-- Drop foreign key constraints
+ALTER TABLE tickets
+    DROP CONSTRAINT IF EXISTS fk_tickets_order_number;
+ALTER TABLE tickets
+    DROP CONSTRAINT IF EXISTS fk_tickets_order_id;
+
 -- Drop indexes
 DROP INDEX IF EXISTS idx_tickets_event_status;
-DROP INDEX IF EXISTS idx_tickets_user_event;
 DROP INDEX IF EXISTS idx_tickets_order_status;
 DROP INDEX IF EXISTS idx_tickets_purchased_at;
 DROP INDEX IF EXISTS idx_tickets_buyer_email;
@@ -19,7 +24,6 @@ DROP INDEX IF EXISTS idx_tickets_status;
 DROP INDEX IF EXISTS idx_tickets_ticket_code;
 DROP INDEX IF EXISTS idx_tickets_order_number;
 DROP INDEX IF EXISTS idx_tickets_order_id;
-DROP INDEX IF EXISTS idx_tickets_user_id;
 DROP INDEX IF EXISTS idx_tickets_event_id;
 
 -- Drop table (cascade will handle foreign key constraints)
